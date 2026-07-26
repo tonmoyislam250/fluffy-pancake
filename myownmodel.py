f745dc269223dc2f8ee6043ffc8e9707b783cd19
Tonmoy Islam <tonmoyk983@gmail.com>
2026-07-26 13:00:22 +0600

    Update myownmodel.py

--- myownmodel.py
+++ myownmodel.py
@@ -201,7 +201,7 @@ else:
 model.eval()
 
 # You can change this to point to the root containing all class folders.
-TESTING_CLASSD2_ROOT = Path('segments/Testing_ClassB/')
+TESTING_CLASSD2_ROOT = Path('segments/Testing_ClassC/')
 INFERENCE_SIZE = 512
 GOP_SIZE = 8
 
@@ -245,7 +245,7 @@ with torch.no_grad():
     _ = model(dummy)
     if DEVICE.type == 'cuda': torch.cuda.synchronize(DEVICE)
 
-out_csv = CKPT_DIR / 'prediction_times_B.csv'
+out_csv = CKPT_DIR / 'prediction_times_C.csv'
 print(f'Saving prediction times to: {out_csv}')
 with open(out_csv, 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
