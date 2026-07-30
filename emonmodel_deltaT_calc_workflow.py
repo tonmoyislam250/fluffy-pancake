7e2dae45b4e46023ba938851329d3b281d95a02e
Tonmoy Islam <tonmoyk983@gmail.com>
2026-07-30 15:45:54 +0600

    Update emonmodel_deltaT_calc_workflow.py

--- emonmodel_deltaT_calc_workflow.py
+++ emonmodel_deltaT_calc_workflow.py
@@ -242,7 +242,7 @@ else:
 model.eval()
 
 # You can change this to point to the root containing all class folders.
-TESTING_CLASSD2_ROOT = Path('segments/Testing_ClassA')
+TESTING_CLASSD2_ROOT = Path('segments/Testing_ClassB')
 INFERENCE_SIZE = 512
 GOP_SIZE = 8
 
@@ -286,7 +286,7 @@ with torch.no_grad():
     _ = model(dummy)
     if DEVICE.type == 'cuda': torch.cuda.synchronize(DEVICE)
 
-out_csv = CKPT_DIR / 'prediction_times_A.csv'
+out_csv = CKPT_DIR / 'prediction_times_B.csv'
 print(f'Saving prediction times to: {out_csv}')
 with open(out_csv, 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
